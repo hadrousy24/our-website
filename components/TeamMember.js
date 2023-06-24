460a8e0fc78501970a6e84065cffe3db3f922c96
Mohammed N. Abuhadrous <abuhadrous.m@gmail.com>
2023-06-24 13:05:24 +0300

    added TeamMember component

---
+++ components/TeamMember.js
@@ -0,0 +1,21 @@
+import styled, { keyframes } from "styled-components";
+
+const TeamMember = ({ memberSize, children }) => {
+  const spin = keyframes`
+    to {
+      transform: rotate(360deg);
+    }`;
+
+  const Member = styled.div`
+    background-color: blue;
+    width: ${memberSize}px;
+    height: ${memberSize}px;
+    animation: ${spin} 4s linear infinite;
+    transform-origin: center;
+    position: absolute;
+  `;
+
+  return <Member>{children}</Member>;
+};
+
+export default TeamMember;
