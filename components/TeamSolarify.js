c1c2716f721e03a6c5c990a7e20eb7868f017294
Mohammed N. Abuhadrous <abuhadrous.m@gmail.com>
2023-06-25 02:26:45 +0300

    bad attempt - 1

--- components/TeamSolarify.js
+++ components/TeamSolarify.js
@@ -1,31 +1,35 @@
 import styled, { keyframes } from "styled-components";
 import TeamMember from "../components/TeamMember";
 
-const TeamSolarify = ({ circleSize }) => {
-  const initialRotation = 0;
+const TeamSolarify = ({ circleSize, members }) => {
+  members.map((member, index) => {
+    const initialRotation = (index * 360) / members.length;
 
-  const revolve = keyframes`
-    0% {
-      transform: rotate(${initialRotation}deg);
-    }
-    100% {
-      transform: rotate(${initialRotation + 360}deg);
-    }`;
+    const revolve = keyframes`
+      0% {
+        transform: rotate(${initialRotation}deg);
+      }
+      100% {
+        transform: rotate(${initialRotation + 360}deg);
+      }`;
 
-  const Circle = styled.div`
-    border: 1px solid red;
-    width: ${circleSize}px;
-    height: ${circleSize}px;
-    animation: ${revolve} 4s linear infinite;
-  `;
+    const Circle = styled.div`
+      border: 1px solid red;
+      width: ${circleSize}px;
+      height: ${circleSize}px;
+      animation: ${revolve} 4s linear infinite;
+    `;
 
-  return (
-    <Circle>
-      <TeamMember initialRotation={initialRotation} memberSize={50}>
-        Hello
-      </TeamMember>
-    </Circle>
-  );
+    return (
+      <div key={member.id}>
+        <Circle>
+          <TeamMember initialRotation={initialRotation} memberSize={50}>
+            {member.name}
+          </TeamMember>
+        </Circle>
+      </div>
+    );
+  });
 };
 
 export default TeamSolarify;
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,32 @@ import styles from "../styles/Home.module.css";
 import TeamSolarify from "../components/TeamSolarify";
 
 export default function Home() {
+  let members = [
+    {
+      image: "https://picsum.photos/20/300",
+      name: "Basel",
+      id: 1,
+    },
+    {
+      image: "https://picsum.photos/20/30",
+      name: "Norhan",
+      id: 2,
+    },
+    {
+      image: "https://picsum.photos/200/30",
+      name: "Mohammed",
+      id: 3,
+    },
+    {
+      image: "https://picsum.photos/200/300",
+      name: "Hadeel",
+      id: 4,
+    },
+  ];
+
   return (
     <div className={styles.container}>
-      <TeamSolarify circleSize={200} />
+      <TeamSolarify circleSize={200} members={members} />
     </div>
   );
 }
